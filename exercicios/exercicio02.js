9cc7658f1a7fc8f759aa38c38f0a8c50c93b4081
PROGRAMAÇÃO EM PYTHON 2025.1 <51129532025.1@senacrjedu.local>
2025-03-14 18:37:30 -0300

    Correcao

---
+++ exercicios/exercicio02.js
@@ -0,0 +1,10 @@
+/* 
+    2. Construa um programa que guarde um valor na variável e o programa
+    escreverá, na tela, o resultado do quadrado deste número. Atenção: utilizar apenas
+    operadores básicos.
+*/
+
+let numero = 10
+let quadrado = numero * numero 
+
+console.log('O quadrado é: ', quadrado)
---
+++ exercicios/exercicio04.js
@@ -0,0 +1,17 @@
+/* 
+    4. Construa um programa que guarde dois números em variáveis diferentes e o programa exibirá, na tela, o resultado das quatro operações básicas da matemática.
+*/
+
+let numUm = 20
+let numDois = 2
+
+let soma = numUm + numDois
+let subtracao = numUm - numDois
+let multiplicacao = numUm * numDois
+let divisao = numUm / numDois
+
+console.log("Soma: ", soma)
+console.log("Subtração: ", subtracao)
+console.log("Multiplicação: ", multiplicacao)
+console.log("Divisão: ", divisao)
+
